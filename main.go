302301b95f88765587a2e5e2a067c981ae1cb5d5
Valentin <valentinlamine@outlook.fr>
2023-03-14 17:37:25 +0100

    Avancement affichage et traitement des données

--- main.go
+++ main.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"net/url"
+	"time"
 )
+
+type Response struct {
+	ResultCount int `json:"resultCount"`
+	Results     []struct {
+		WrapperType             string    `json:"wrapperType"`
+		Kind                    string    `json:"kind"`
+		ArtistID                int       `json:"artistId"`
+		CollectionID            int       `json:"collectionId"`
+		TrackID                 int       `json:"trackId"`
+		ArtistName              string    `json:"artistName"`
+		CollectionName          string    `json:"collectionName"`
+		TrackName               string    `json:"trackName"`
+		CollectionCensoredName  string    `json:"collectionCensoredName"`
+		TrackCensoredName       string    `json:"trackCensoredName"`
+		ArtistViewURL           string    `json:"artistViewUrl"`
+		CollectionViewURL       string    `json:"collectionViewUrl"`
+		TrackViewURL            string    `json:"trackViewUrl"`
+		PreviewURL              string    `json:"previewUrl"`
+		ArtworkURL30            string    `json:"artworkUrl30"`
+		ArtworkURL60            string    `json:"artworkUrl60"`
+		ArtworkURL100           string    `json:"artworkUrl100"`
+		CollectionPrice         float64   `json:"collectionPrice"`
+		TrackPrice              float64   `json:"trackPrice"`
+		ReleaseDate             time.Time `json:"releaseDate"`
+		CollectionExplicitness  string    `json:"collectionExplicitness"`
+		TrackExplicitness       string    `json:"trackExplicitness"`
+		DiscCount               int       `json:"discCount"`
+		DiscNumber              int       `json:"discNumber"`
+		TrackCount              int       `json:"trackCount"`
+		TrackNumber             int       `json:"trackNumber"`
+		TrackTimeMillis         int       `json:"trackTimeMillis"`
+		Country                 string    `json:"country"`
+		Currency                string    `json:"currency"`
+		PrimaryGenreName        string    `json:"primaryGenreName"`
+		IsStreamable            bool      `json:"isStreamable,omitempty"`
+		CollectionArtistName    string    `json:"collectionArtistName,omitempty"`
+		CollectionArtistID      int       `json:"collectionArtistId,omitempty"`
+		ContentAdvisoryRating   string    `json:"contentAdvisoryRating,omitempty"`
+		ShortDescription        string    `json:"shortDescription,omitempty"`
+		LongDescription         string    `json:"longDescription,omitempty"`
+		CollectionArtistViewURL string    `json:"collectionArtistViewUrl,omitempty"`
+	} `json:"results"`
+}
 
 func main() {
 	fmt.Println("Server started on port 80 : http://localhost")
@@ -28,9 +74,39 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("templates/recherche.html")
-	t.Execute(w, nil)
+	if r.Method == "POST" {
+		request := API_GET(r.FormValue("search"))
+		t.Execute(w, request.Results)
+	} else {
+		t.Execute(w, nil)
+	}
+
 }
 
+func API_GET(search string) Response {
+	url := "https://itunes.apple.com/search?term=" + url.QueryEscape(search)
+
+	req, _ := http.NewRequest("GET", url, nil) //Création de la requête
+
+	res, err := http.DefaultClient.Do(req) //Envoi de la requête
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()          //Fermeture du body
+	body, _ := io.ReadAll(res.Body) //Lecture du body
+
+	var request Response           //Création de la variable temporaire
+	json.Unmarshal(body, &request) //Désérialisation du JSON
+
+	fmt.Println("Requette effectuée avec ", request.ResultCount, " résultats") //Affichage du nombre de résultats
+	fmt.Println(len(request.Results))
+
+	return request //Retourne la variable temporaire
+}
+
+/*
 func API_TEST() {
 	url := "https://randomuser.me/api/"
 
@@ -49,3 +125,4 @@ func API_TEST() {
 	}
 
 }
+*/
