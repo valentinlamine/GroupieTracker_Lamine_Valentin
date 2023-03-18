1b9845177ac0af7748cee1f3d46f1643c6cc8536
valentinlamine <87501303+valentinlamine@users.noreply.github.com>
2023-03-18 16:03:28 +0100

    Finition du responsive

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,7 +1,12 @@
 function ResizeCover() {
     var cover = document.getElementById("book_cover");
     var description = document.getElementById("book_description");
-    cover.style.maxHeight = description.offsetHeight + "px";
+    //Si la taille d'écran est supérieur à 991px
+    if (window.innerWidth > 991) {
+        cover.style.maxHeight = description.offsetHeight + "px";
+    } else {
+        cover.style.maxHeight = "300px";
+    }
 }
 
 window.onload = ResizeCover;
